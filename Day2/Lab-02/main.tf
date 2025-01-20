507135b6d37153a157daeb8d843e252dadf16267
root <root@instance-test.us-central1-b.c.te-environment-448317.internal>
2025-01-20 21:00:29 +0000

    Practiced and Completed Lab 2...

--- Day2/Lab-02/main.tf
+++ Day2/Lab-02/main.tf
@@ -1,47 +1,65 @@
-resource "aws_vpc" "myvpc" {
+# Define the VPC
+resource "aws_vpc" "test-vpc" {
   cidr_block = var.cidr
 }
 
-resource "aws_subnet" "sub1" {
-  vpc_id                  = aws_vpc.myvpc.id
-  cidr_block              = "10.0.0.0/24"
-  availability_zone       = "us-east-1a"
+# Define Subnets
+resource "aws_subnet" "test-subnet-01" {
+  vpc_id                  = aws_vpc.test-vpc.id
+  cidr_block              = "10.52.0.0/24"
+  availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 }
 
-resource "aws_subnet" "sub2" {
-  vpc_id                  = aws_vpc.myvpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1b"
+resource "aws_subnet" "test-subnet-02" {
+  vpc_id                  = aws_vpc.test-vpc.id
+  cidr_block              = "10.52.1.0/24"
+  availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = true
 }
 
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.myvpc.id
+resource "aws_subnet" "test-subnet-03" {
+  vpc_id                  = aws_vpc.test-vpc.id
+  cidr_block              = "10.52.2.0/24"
+  availability_zone       = "ap-south-1c"
+  map_public_ip_on_launch = true
 }
 
-resource "aws_route_table" "RT" {
-  vpc_id = aws_vpc.myvpc.id
+# Define the Internet Gateway
+resource "aws_internet_gateway" "test-igw" {
+  vpc_id = aws_vpc.test-vpc.id
+}
+
+# Define the Route Table
+resource "aws_route_table" "test-rt" {
+  vpc_id = aws_vpc.test-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.test-igw.id
   }
 }
 
+# Associate Route Table with Subnets
 resource "aws_route_table_association" "rta1" {
-  subnet_id      = aws_subnet.sub1.id
-  route_table_id = aws_route_table.RT.id
+  subnet_id      = aws_subnet.test-subnet-01.id
+  route_table_id = aws_route_table.test-rt.id
 }
 
 resource "aws_route_table_association" "rta2" {
-  subnet_id      = aws_subnet.sub2.id
-  route_table_id = aws_route_table.RT.id
+  subnet_id      = aws_subnet.test-subnet-02.id
+  route_table_id = aws_route_table.test-rt.id
 }
 
-resource "aws_security_group" "webSg" {
-  name   = "web"
-  vpc_id = aws_vpc.myvpc.id
+resource "aws_route_table_association" "rta3" {
+  subnet_id      = aws_subnet.test-subnet-03.id
+  route_table_id = aws_route_table.test-rt.id
+}
+
+# Define Security Group
+resource "aws_security_group" "test-sg" {
+  name   = "test-sg"
+  vpc_id = aws_vpc.test-vpc.id
 
   ingress {
     description = "HTTP from VPC"
@@ -50,11 +68,28 @@ resource "aws_security_group" "webSg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   ingress {
     description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "All ICMP - IPv4"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -66,50 +101,56 @@ resource "aws_security_group" "webSg" {
   }
 
   tags = {
-    Name = "Web-sg"
+    Name = "test-sg"
   }
 }
 
+# S3 Bucket
 resource "aws_s3_bucket" "example" {
-  bucket = "abhisheksterraform2023project"
+  bucket = "tedevterraform2025project"
 }
 
-
+# EC2 Instances
 resource "aws_instance" "webserver1" {
-  ami                    = "ami-0261755bbcb8c4a84"
+  ami                    = "ami-023a307f3d27ea427"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.webSg.id]
-  subnet_id              = aws_subnet.sub1.id
+  vpc_security_group_ids = [aws_security_group.test-sg.id]
+  subnet_id              = aws_subnet.test-subnet-02.id
   user_data              = base64encode(file("userdata.sh"))
 }
 
 resource "aws_instance" "webserver2" {
-  ami                    = "ami-0261755bbcb8c4a84"
+  ami                    = "ami-023a307f3d27ea427"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.webSg.id]
-  subnet_id              = aws_subnet.sub2.id
+  vpc_security_group_ids = [aws_security_group.test-sg.id]
+  subnet_id              = aws_subnet.test-subnet-02.id
   user_data              = base64encode(file("userdata1.sh"))
 }
 
-#create alb
-resource "aws_lb" "myalb" {
-  name               = "myalb"
+# Create ALB
+resource "aws_lb" "test-alb" {
+  name               = "test-alb"
   internal           = false
   load_balancer_type = "application"
 
-  security_groups = [aws_security_group.webSg.id]
-  subnets         = [aws_subnet.sub1.id, aws_subnet.sub2.id]
+  security_groups = [aws_security_group.test-sg.id]
+  subnets         = [
+    aws_subnet.test-subnet-01.id,
+    aws_subnet.test-subnet-02.id,
+    aws_subnet.test-subnet-03.id
+  ]
 
   tags = {
     Name = "web"
   }
 }
 
-resource "aws_lb_target_group" "tg" {
-  name     = "myTG"
+# Target Group
+resource "aws_lb_target_group" "test-tg" {
+  name     = "test-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.myvpc.id
+  vpc_id   = aws_vpc.test-vpc.id
 
   health_check {
     path = "/"
@@ -117,29 +158,32 @@ resource "aws_lb_target_group" "tg" {
   }
 }
 
+# Attach Instances to Target Group
 resource "aws_lb_target_group_attachment" "attach1" {
-  target_group_arn = aws_lb_target_group.tg.arn
+  target_group_arn = aws_lb_target_group.test-tg.arn
   target_id        = aws_instance.webserver1.id
   port             = 80
 }
 
 resource "aws_lb_target_group_attachment" "attach2" {
-  target_group_arn = aws_lb_target_group.tg.arn
+  target_group_arn = aws_lb_target_group.test-tg.arn
   target_id        = aws_instance.webserver2.id
   port             = 80
 }
 
-resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.myalb.arn
+# ALB Listener
+resource "aws_lb_listener" "test-listener" {
+  load_balancer_arn = aws_lb.test-alb.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
-    target_group_arn = aws_lb_target_group.tg.arn
+    target_group_arn = aws_lb_target_group.test-tg.arn
     type             = "forward"
   }
 }
 
+# Output the Load Balancer DNS Name
 output "loadbalancerdns" {
-  value = aws_lb.myalb.dns_name
+  value = aws_lb.test-alb.dns_name
 }
--- Day2/Lab-02/provider.tf
+++ Day2/Lab-02/provider.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "5.11.0"
+      source = "hashicorp/aws"
+      version = "5.84.0"
     }
   }
 }
 
 provider "aws" {
   # Configuration options
-  region = "us-east-1"
+  region = "ap-south-1"
 }
